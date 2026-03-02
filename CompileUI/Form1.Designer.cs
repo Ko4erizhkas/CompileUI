d56c07e9a53c295263083a4fccf18418e6057707
Ko4erizhkas <fit12347@gmail.com>
2026-03-02 14:32:31 +0700

    Добавьте файлы проекта.

---
+++ CompileUI/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CompileUI
+{
+    partial class Compiler
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Compiler));
+            textBox2 = new TextBox();
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem1 = new ToolStripMenuItem();
+            правкаToolStripMenuItem = new ToolStripMenuItem();
+            текстToolStripMenuItem = new ToolStripMenuItem();
+            пускToolStripMenuItem = new ToolStripMenuItem();
+            справкаToolStripMenuItem = new ToolStripMenuItem();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            textBox1 = new TextBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(11, 277);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(814, 160);
+            textBox2.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Font = new Font("Webdings", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 2);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem1, правкаToolStripMenuItem, текстToolStripMenuItem, пускToolStripMenuItem, справкаToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(6, 3, 0, 3);
+            menuStrip1.Size = new Size(837, 38);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem1
+            // 
+            файлToolStripMenuItem1.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold);
+            файлToolStripMenuItem1.Name = "файлToolStripMenuItem1";
+            файлToolStripMenuItem1.Size = new Size(75, 32);
+            файлToolStripMenuItem1.Text = "Файл";
+            // 
+            // правкаToolStripMenuItem
+            // 
+            правкаToolStripMenuItem.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold);
+            правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            правкаToolStripMenuItem.Size = new Size(95, 32);
+            правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // текстToolStripMenuItem
+            // 
+            текстToolStripMenuItem.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold);
+            текстToolStripMenuItem.Name = "текстToolStripMenuItem";
+            текстToolStripMenuItem.Size = new Size(77, 32);
+            текстToolStripMenuItem.Text = "Текст";
+            // 
+            // пускToolStripMenuItem
+            // 
+            пускToolStripMenuItem.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold);
+            пускToolStripMenuItem.Name = "пускToolStripMenuItem";
+            пускToolStripMenuItem.Size = new Size(71, 32);
+            пускToolStripMenuItem.Text = "Пуск";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            справкаToolStripMenuItem.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold);
+            справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            справкаToolStripMenuItem.Size = new Size(104, 32);
+            справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(11, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(70, 56);
+            button1.TabIndex = 5;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(91, 43);
+            button2.Name = "button2";
+            button2.Size = new Size(70, 56);
+            button2.TabIndex = 6;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(168, 43);
+            button3.Name = "button3";
+            button3.Size = new Size(70, 56);
+            button3.TabIndex = 7;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(245, 43);
+            button4.Name = "button4";
+            button4.Size = new Size(59, 56);
+            button4.TabIndex = 8;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(310, 43);
+            button5.Name = "button5";
+            button5.Size = new Size(59, 56);
+            button5.TabIndex = 9;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.Location = new Point(376, 43);
+            button6.Name = "button6";
+            button6.Size = new Size(70, 56);
+            button6.TabIndex = 10;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.Location = new Point(453, 43);
+            button7.Name = "button7";
+            button7.Size = new Size(70, 56);
+            button7.TabIndex = 11;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Image = (Image)resources.GetObject("button8.Image");
+            button8.Location = new Point(528, 43);
+            button8.Name = "button8";
+            button8.Size = new Size(70, 56);
+            button8.TabIndex = 12;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Image = (Image)resources.GetObject("button9.Image");
+            button9.Location = new Point(603, 43);
+            button9.Name = "button9";
+            button9.Size = new Size(70, 56);
+            button9.TabIndex = 13;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Image = (Image)resources.GetObject("button10.Image");
+            button10.Location = new Point(680, 43);
+            button10.Name = "button10";
+            button10.Size = new Size(70, 56);
+            button10.TabIndex = 14;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Image = (Image)resources.GetObject("button11.Image");
+            button11.Location = new Point(757, 43);
+            button11.Name = "button11";
+            button11.Size = new Size(70, 56);
+            button11.TabIndex = 15;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(8, 104);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(814, 160);
+            textBox1.TabIndex = 0;
+            // 
+            // Compiler
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(837, 451);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            MainMenuStrip = menuStrip1;
+            Name = "Compiler";
+            Text = "Compiler";
+            Load += Compiler_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem1;
+        private ToolStripMenuItem правкаToolStripMenuItem;
+        private ToolStripMenuItem текстToolStripMenuItem;
+        private ToolStripMenuItem пускToolStripMenuItem;
+        private ToolStripMenuItem справкаToolStripMenuItem;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private TextBox textBox1;
+    }
+}
---
+++ CompileUI/Form1.cs
@@ -0,0 +1,19 @@
+namespace CompileUI;
+
+public partial class Compiler : Form
+{
+    public Compiler()
+    {
+        InitializeComponent();
+    }
+
+    private void Compiler_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ‚ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+    }
+}
